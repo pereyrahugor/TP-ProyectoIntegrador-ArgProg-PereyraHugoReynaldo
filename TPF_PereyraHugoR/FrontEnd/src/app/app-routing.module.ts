6ff96330a7e3bfc05b84eee7ffab6c6ce23dd340
= <62813900+pereyrahugor@users.noreply.github.com>
2022-07-08 11:44:11 -0300

    Error 404
    Avanzando

--- TPF_PereyraHugoR/FrontEnd/src/app/app-routing.module.ts
+++ TPF_PereyraHugoR/FrontEnd/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './components/error404/error404.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
    {path:'', component: HomeComponent},
    {path:'login', component: LoginComponent},
-   {path:'*', component: LoginComponent},
+   {path: '**', component: Error404Component}
 ];
 
 @NgModule({
--- TPF_PereyraHugoR/FrontEnd/src/app/components/error404/error404.component.ts
+++ TPF_PereyraHugoR/FrontEnd/src/app/components/error404/error404.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error404',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Error404Component implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  volver(){
+    this.router.navigate(['/'])
+  }
 }
